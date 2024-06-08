b95a95e171b01490c3876a11f37944320b19c707
Ibraheemosule <ibraheemosule@gmail.com>
2024-06-08 17:46:46 +0100

    improved the validation for posting an announcement

--- api/announcements/validations.py
+++ api/announcements/validations.py
@@ -5,31 +5,20 @@ from utils.custom_error import CustomError
 
 def announcements_validation(payload: TAnnouncementPayload):
     keys = TAnnouncementPayload.__annotations__.keys()
-    pass
-
     errors = []
 
-    if payload['type'] == 'memo':
-        for k in keys:
-            if k in ['event_start_date', 'event_end_date', 'event_time']:
-                errors.append(f"{k} is an invalid payload")
-            elif payload.get(k) is None:
-                errors.append(f"{k} is empty")
-    
-    if payload['type'] == 'single_event':
-        for k in keys:
-            if k == 'event_end_date':
-                 errors.append(f"{k} is an invalid payload")
-            elif payload.get(k) is None:
-                errors.append(f"{k} is empty")
-            
-    if payload['type'] == 'multi_event':
+    invalid_fields = {
+        'memo': {'event_start_date', 'event_end_date', 'event_time'},
+        'single_event': {'event_end_date'},
+        'multi_event': {'event_time'}
+    }
 
-        for k in keys:
-            if k == 'event_time':
-                  errors.append(f"{k} is an invalid payload")
-            elif payload.get(k) is None:
-                errors.append(f"{k} is empty")
+    for k in keys:
+        if k in invalid_fields.get(payload['type'], {}):
+            if k in payload:
+                errors.append(f"{k} is an invalid payload")
+        elif payload.get(k) is None:
+            errors.append(f"{k} is empty")
     
     if len(errors):
         raise CustomError(', '.join(errors), 403)
